85d58aebda568e63ad23014a2283ee37e7303322
widesea30 <widesea392@yahoo.com>
2021-06-22 20:24:22 +0200

    update home page style

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,16 @@ module.exports = {
         gray4: '#F0F0F0',
         gray5: '#F7E4E6',
         green1: '#94E866',
+      },
+      opacity: {
+        '8': '0.08',
+        '55': '0.55',
+      },
+      borderOpacity: {
+        '8': '0.08'
+      },
+      textOpacity: {
+        '55': '0.55',
       }
     },
   },
